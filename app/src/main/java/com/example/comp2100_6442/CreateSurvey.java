88689e13076a3bd13565e9679aa0a38c9d5f7165
Mithun Comar <u6849970@anu.edu.au>
2020-05-25 02:12:21 +1000

    Basic survey page

--- app/src/main/java/com/example/comp2100_6442/CreateSurvey.java
+++ app/src/main/java/com/example/comp2100_6442/CreateSurvey.java
@@ -5,29 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class CreateSurvey extends AppCompatActivity {
+public class CreateSurvey extends AppCompatActivity implements View.OnClickListener {
+
+    EditText mEtNumOfRadioBtns;
+    Button mBtnAdd;
+    String TAG = "TestActivity";
+    RadioGroup mRgAllButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_create_survey);
+        //
+       // mEtNumOfRadioBtns = findViewById(R.id.et_no);
+        mBtnAdd = findViewById(R.id.btn);
+        mRgAllButtons = findViewById(R.id.radiogroup);
+        //
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearlayout);
-
-        for(int i = 0; i<5; i++){
-            EditText editText = new EditText(this);
-            editText.setText(String.valueOf(i));
-            linearLayout.addView(editText);
-
-        }
-
-
+               int number = 1;/*Integer.parseInt(mEtNumOfRadioBtns.getText().toString().trim());*/
+                addRadioButtons(number);
+            }
+        });
     }
 
+    public void addRadioButtons(int number) {
+        mRgAllButtons.setOrientation(LinearLayout.VERTICAL);
+        //
+        int x = 0;
+        x = number;
+        for (int i = 1; i <= x; i++) {
+            RadioButton rdbtn = new RadioButton(this);
 
+            rdbtn.setId(View.generateViewId());
+            rdbtn.setText("Option " + rdbtn.getId());
+            rdbtn.setOnClickListener(this);
+            mRgAllButtons.addView(rdbtn);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, " Name " + ((RadioButton) v).getText() + " Id is " + v.getId());
+    }
 }
--- app/src/main/java/com/example/comp2100_6442/Surveylist.java
+++ app/src/main/java/com/example/comp2100_6442/Surveylist.java
@@ -6,26 +6,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Surveylist extends AppCompatActivity {
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surveylist);
         this.setTitle("Survey List");
+        rb1 = findViewById(R.id.MCQ);
+        rb2 = findViewById(R.id.Subjective);
+        rb3 = findViewById(R.id.CreateYourOwn);
 
         SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
         String show = preferences.getString("show", "");
 
         TextView display = (TextView) findViewById(R.id.successView);
         display.setText(show);
+
     }
     public void NextPressed(View view) {
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
+        if( rb1.isChecked()) {
+            Intent intent = new Intent(getApplicationContext(), CreateSurvey.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
 
+        }
         SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
         preferences.edit().clear().commit();
     }
