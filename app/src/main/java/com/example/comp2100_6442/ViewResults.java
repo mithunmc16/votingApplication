8f4a892f7cd63367d863f2fa2cef61caef0e299d
Veronica Chow <u7087149@anu.edu.au>
2020-05-27 21:13:01 +1000

    - Added empty "ViewResults" activity - Added "Anychart" dependencies @ build.gradle (project) and build.gradle (module)

---
+++ app/src/main/java/com/example/comp2100_6442/ViewResults.java
@@ -0,0 +1,14 @@
+package com.example.comp2100_6442;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_results);
+    }
+}
