8f017e718e8b4f7ca48eb9a17918d9f2833e603e
Mithun Comar <u6849970@anu.edu.au>
2020-05-29 02:28:15 +1000

    voting page

--- app/src/main/java/com/example/comp2100_6442/QuestionsActivity.java
+++ app/src/main/java/com/example/comp2100_6442/QuestionsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.animation.Animator;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
@@ -11,7 +13,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -19,6 +22,8 @@ public class QuestionsActivity extends AppCompatActivity {
     private LinearLayout optionsContainer;
     private Button shareBtn, nextBtn;
     private int count = 0;
+    private List<SurveyQuestionModel> list;
+    private int pos = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,38 +33,81 @@ public class QuestionsActivity extends AppCompatActivity {
 
 
         question = findViewById(R.id.question);
-       noIndicator = findViewById(R.id.no_indicator);
+        noIndicator = findViewById(R.id.no_indicator);
         optionsContainer = findViewById(R.id.options_container);
         shareBtn = findViewById(R.id.sharebtn);
         nextBtn = findViewById(R.id.nextbtn);
 
+        list = new ArrayList<>();
+        list.add(new SurveyQuestionModel("question1","option A", "Option B", "Option C", "Option D" ));
+        list.add(new SurveyQuestionModel("question2","option A", "Option A", "Option A", "Option A" ));
+        list.add(new SurveyQuestionModel("question3","option C", "Option C", "Option C", "Option C" ));
+        list.add(new SurveyQuestionModel("question4","option D", "Option D", "Option D", "Option D" ));
+        list.add(new SurveyQuestionModel("question5","option D", "Option B", "Option C", "Option A" ));
+
+        for(int i = 0; i<4; i++){
+            optionsContainer.getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkoption(((Button)v));
+                }
+            });
+        }
+
+        playAnimation(question, 0, list.get(pos).getQuestion());
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nextBtn.setEnabled(false);
+                nextBtn.setAlpha(0.6f);
+                enableoption(true);
+                pos++;
+                if(pos == list.size()){
+                    return;
+                }
                 count = 0;
-                playAnimation(question,0);
+                playAnimation(question,0,list.get(pos).getQuestion());
             }
         });
 
     }
-    private void playAnimation(final View view, final int value){
+    private void playAnimation(final View view, final int value, final String data){
         view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(500).setStartDelay(100)
                 .setInterpolator(new DecelerateInterpolator()).setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
                 if(value==0 && count<4){
-                playAnimation(optionsContainer.getChildAt(count),0);
-                count++;
+                    String option ="";
+                    if(count == 0) {
+                        option = list.get(pos).getOptionA();
+
+                    }else if(count == 1){
+                        option = list.get(pos).getOptionB();
+                    }else if(count == 2){
+                        option = list.get(pos).getOptionC();
+                    }
+                    else if(count == 3){
+                        option = list.get(pos).getOptionD();
+                    }
+                    playAnimation(optionsContainer.getChildAt(count),0, option );
+                    count++;
                 }
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
 
-            if(value == 0){
-                playAnimation(view,1);
 
-            }
+                if(value == 0){
+                    try{
+                        ((TextView)view).setText(data);
+                        noIndicator.setText(pos+1+"/"+list.size());
+                    }catch (Exception e){
+                        ((Button)view).setText(data);
+                    }
+                    playAnimation(view,1, data);
+
+                }
             }
 
             @Override
@@ -72,5 +120,23 @@ public class QuestionsActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void checkoption(Button selectedoption){
+
+        enableoption(false);
+        nextBtn.setEnabled(true);
+        nextBtn.setAlpha(1);
+        selectedoption.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#4CAF5C")));
+    }
+    private void enableoption(boolean enable) {
+        for (int i = 0; i < 4; i++) {
+            optionsContainer.getChildAt(i).setEnabled(enable);
+            if(enable == true){
+                optionsContainer.getChildAt(i).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#989898")));
+
+
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/comp2100_6442/SurveyQuestionModel.java
@@ -0,0 +1,53 @@
+package com.example.comp2100_6442;
+
+public class SurveyQuestionModel {
+    private String question, optionA, optionB, optionC, optionD;
+
+    public SurveyQuestionModel(String question, String optionA, String optionB, String optionC, String oprionD) {
+        this.question = question;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = oprionD;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOprionD(String optionD) {
+        this.optionD = optionD;
+    }
+}
